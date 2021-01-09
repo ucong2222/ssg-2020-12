002737c4de612f6ddf68def7335525432d953e77
ucong2222 <dbrudrjf21@gmail.com>
2021-01-10 01:00:15 +0900

    배포

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ function ArticleDetail__Body__init() {
 		return;
 	}
 
-	var body = document.querySelector('.article-detail__body');
-	var initialValue = body.innerHTML.trim();
-
-	var viewer = new toastui.Editor.factory({
-		el: body,
-		initialValue: initialValue,
-		viewer: true,
-		plugins: [toastui.Editor.plugin.codeSyntaxHighlight,youtubePlugin]
-	});
-
 	// 유튜브 플러그인 시작
 	function youtubePlugin() {
 		toastui.Editor.codeBlockManager.setReplacer('youtube', youtubeId => {
@@ -33,6 +23,15 @@ function ArticleDetail__Body__init() {
 		el.innerHTML = `<div class="toast-ui-youtube-plugin-wrap"><iframe src="https://www.youtube.com/embed/${youtubeId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>`;
 	}
 	// 유튜브 플러그인 끝
+	var body = document.querySelector('.article-detail__body');
+	var initialValue = body.innerHTML.trim();
+	
+	var viewer = new toastui.Editor.factory({
+		el: body,
+		initialValue: initialValue,
+		viewer: true,
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight,youtubePlugin]
+	});
 }
 
 ArticleDetail__Body__init();
